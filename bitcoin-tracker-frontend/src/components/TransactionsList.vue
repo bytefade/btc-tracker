834e714ed789689a51400c9ffd4720eab5390b97
Nailton Sousa <nailton.osn@gmail.com>
2025-10-28 19:21:10 -0300

    Update TransactionsList.vue

--- bitcoin-tracker-frontend/src/components/TransactionsList.vue
+++ bitcoin-tracker-frontend/src/components/TransactionsList.vue
@@ -18,91 +18,107 @@
       </button>
       <!-- <button>Export CSV</button> -->
     </div>
-    <div class="overflow-x-auto"></div>
-    <table class="min-w-full border border-gray-200">
-      <thead class="bg-orange-50">
-        <tr>
-          <th class="py-2 px-4 text-left text-gray-700 font-semibold">Tipo</th>
-          <th class="py-2 px-4 text-left text-gray-700 font-semibold">Data</th>
-          <th class="py-2 px-4 text-left text-gray-700 font-semibold">BTC</th>
-          <th class="py-2 px-4 text-left text-gray-700 font-semibold">Preço</th>
-          <th class="py-2 px-4 text-left text-gray-700 font-semibold">Taxa</th>
-          <th class="py-2 px-4 text-left text-gray-700 font-semibold">Total</th>
-          <th class="py-2 px-4 text-left text-gray-700 font-semibold">Notas</th>
-          <th class="py-2 px-4 text-left text-gray-700 font-semibold">Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="t in transactions" :key="t._id" class="border-t">
-          <td
-            class="py-2 px-4"
-            :class="t.type === 'compra' ? 'text-green-600' : 'text-red-600'"
-          >
-            {{ t.type.charAt(0).toUpperCase() + t.type.slice(1) }}
-          </td>
-          <td class="py-2 px-4">
-            {{
-              new Date(t.date).toLocaleDateString("pt-BR", { timeZone: "UTC" })
-            }}
-          </td>
-          <td class="py-2 px-4">{{ formatBtc(t.btcAmount) }}</td>
-          <td class="py-2 px-4">
-            {{
-              t.brlPricePerBtc.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2,
-              })
-            }}
-          </td>
-          <td class="py-2 px-4">
-            {{
-              t.brokerageFee.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-                minimumFractionsDigits: 2,
-                maximumFractionDigits: 2,
-              })
-            }}
-          </td>
-          <td class="py-2 px-4">
-            {{
-              t.totalBrl.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-                minimumFractionsDigits: 2,
-                maximumFractionDigits: 2,
-              })
-            }}
-          </td>
-          <td class="py-2 px-4">{{ t.notes || "-" }}</td>
-          <td class="py-2 px-4">
-            <button
-              @click="openDeleteModal(t._id)"
-              class="text-red-600 hover:text-red-800 transition duration-200"
-              title="Excluir transação"
+    <div class="overflow-x-auto">
+      <table class="min-w-full border border-gray-200">
+        <thead class="bg-orange-50">
+          <tr>
+            <th class="py-2 px-4 text-left text-gray-700 font-semibold">
+              Tipo
+            </th>
+            <th class="py-2 px-4 text-left text-gray-700 font-semibold">
+              Data
+            </th>
+            <th class="py-2 px-4 text-left text-gray-700 font-semibold">BTC</th>
+            <th class="py-2 px-4 text-left text-gray-700 font-semibold">
+              Preço
+            </th>
+            <th class="py-2 px-4 text-left text-gray-700 font-semibold">
+              Taxa
+            </th>
+            <th class="py-2 px-4 text-left text-gray-700 font-semibold">
+              Total
+            </th>
+            <th class="py-2 px-4 text-left text-gray-700 font-semibold">
+              Notas
+            </th>
+            <th class="py-2 px-4 text-left text-gray-700 font-semibold">
+              Ações
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="t in transactions" :key="t._id" class="border-t">
+            <td
+              class="py-2 px-4"
+              :class="t.type === 'compra' ? 'text-green-600' : 'text-red-600'"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+              {{ t.type.charAt(0).toUpperCase() + t.type.slice(1) }}
+            </td>
+            <td class="py-2 px-4">
+              {{
+                new Date(t.date).toLocaleDateString("pt-BR", {
+                  timeZone: "UTC",
+                })
+              }}
+            </td>
+            <td class="py-2 px-4">{{ formatBtc(t.btcAmount) }}</td>
+            <td class="py-2 px-4">
+              {{
+                t.brlPricePerBtc.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })
+              }}
+            </td>
+            <td class="py-2 px-4">
+              {{
+                t.brokerageFee.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                  minimumFractionsDigits: 2,
+                  maximumFractionDigits: 2,
+                })
+              }}
+            </td>
+            <td class="py-2 px-4">
+              {{
+                t.totalBrl.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                  minimumFractionsDigits: 2,
+                  maximumFractionDigits: 2,
+                })
+              }}
+            </td>
+            <td class="py-2 px-4">{{ t.notes || "-" }}</td>
+            <td class="py-2 px-4">
+              <button
+                @click="openDeleteModal(t._id)"
+                class="text-red-600 hover:text-red-800 transition duration-200"
+                title="Excluir transação"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  />
+                </svg>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <!-- Modal de Confirmação -->
     <DeleteModal
       :is-open="showDeleteModal"
